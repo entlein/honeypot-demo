9dad2c5799394a0037082423f8cfe700b5618182
entlein <einentlein@gmail.com>
2024-02-08 12:17:46 +0100

    chore: some cleanup

--- jupyterhub/consume.py
+++ jupyterhub/consume.py
@@ -23,5 +23,31 @@ try:
 except KeyboardInterrupt:
     pass
 
+
+policy_name_count = 0
+successful_ssh_connection_count = 0
+
+try:
+    while True:
+        msg = c.poll(1.0)  # Wait for up to 1.0 seconds for a message
+        if msg is None:
+            continue
+        if msg.error():
+            raise KafkaException(msg.error())
+        else:
+            # Proper message
+            message = msg.value().decode('utf-8')
+            if 'policy_name' in message:
+                policy_name_count += 1
+            if 'successful_ssh_connection' in message:
+                successful_ssh_connection_count += 1
+
+except KeyboardInterrupt:
+    pass
+
+
 finally:
     c.close()
+
+print('Number of messages containing "policy_name":', policy_name_count)
+print('Number of messages containing "successful_ssh_connection":', successful_ssh_connection_count)
